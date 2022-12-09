6f3364c57d05679d2f499fae2a47d8563ac955f1
Julia Titkova <juli.ogogo@yandex.ru>
2022-12-09 17:43:23 +0300

    SuperEditableSpan, SuperRadio, SuperSelect added

--- src/n2-features/f0-test/pages/TestStand.tsx
+++ src/n2-features/f0-test/pages/TestStand.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import SuperInputText from "../../../n1-main/m1-ui/common/c1-SuperInputText/SuperInputText";
 import SuperButton from "../../../n1-main/m1-ui/common/c2-SuperButton/SuperButton";
 import SuperCheckbox from "../../../n1-main/m1-ui/common/c3-SuperCheckbox/SuperCheckbox";
 import s from './TestStand.module.css'
+import SuperEditableSpan from "../../../n1-main/m1-ui/common/c4-SuperEditableSpan/SuperEditableSpan";
 
 type TestStandPropsType = {}
 
 export const TestStand: React.FC<TestStandPropsType> = ({}) => {
+
+    const [value, setValue] = useState('')
+
     return (
         <div className={s.testStand}>
             <div className={s.text}>
@@ -24,6 +28,14 @@ export const TestStand: React.FC<TestStandPropsType> = ({}) => {
                 <SuperCheckbox>
                     Click me too!
                 </SuperCheckbox>
+
+                <SuperEditableSpan
+                    value={value}
+                    onChangeText={setValue}
+                    spanProps={{
+                        defaultText: 'enter text...',
+                    }}
+                />
             </div>
         </div>
     )
