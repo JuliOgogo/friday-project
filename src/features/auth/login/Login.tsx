abbcd80d73856f0224c3cc2256610f59345824c9
Julia Titkova <juli.ogogo@yandex.ru>
2022-12-13 14:14:39 +0300

    merge login page

--- src/features/auth/login/Login.tsx
+++ src/features/auth/login/Login.tsx
@@ -1,20 +1,24 @@
-import React from "react";
-import {LoginForm} from "./loginForm/LoginForm";
-import {Paper, Typography} from "@mui/material";
-import style from "./Login.module.css";
-import {useAppSelector} from "../../../app/store";
-import {Navigate} from "react-router-dom";
+import React from 'react'
+
+import { Paper, Typography } from '@mui/material'
+import { Navigate } from 'react-router-dom'
+
+import { useAppSelector } from '../../../app/store'
+
+import style from './Login.module.css'
+import { LoginForm } from './loginForm/LoginForm'
 
 export const Login = () => {
-    const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn);
+  const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
 
-    if (isLoggedIn) return <Navigate to={"/profile"}/>;
-    return (
-        <Paper elevation={3} className={style.loginContainer}>
-            <Typography variant="h4" className={style.title}>
-                Sing In
-            </Typography>
-            <LoginForm/>
-        </Paper>
-    );
-};
+  if (isLoggedIn) return <Navigate to={'/profile'} />
+
+  return (
+    <Paper elevation={3} className={style.loginContainer}>
+      <Typography variant="h4" className={style.title}>
+        Sing In
+      </Typography>
+      <LoginForm />
+    </Paper>
+  )
+}
--- src/features/auth/login/loginForm/LoginForm.tsx
+++ src/features/auth/login/loginForm/LoginForm.tsx
@@ -1,97 +1,109 @@
-import React from "react";
-import style from "./LoginForm.module.css"
-import {useFormik} from "formik";
-import {Button, Checkbox, FormControlLabel, FormGroup, TextField} from "@mui/material";
-import {NavLink} from "react-router-dom";
-import {useAppDispatch} from "../../../../app/store";
-import {loginTC} from "../../auth-reducer";
-import {LoginDataType} from "../../auth-api";
+import React from 'react'
 
+import { Button, Checkbox, FormControlLabel, FormGroup, TextField } from '@mui/material'
+import { useFormik } from 'formik'
+import { NavLink } from 'react-router-dom'
+
+import { useAppDispatch } from '../../../../app/store'
+import { LoginDataType } from '../../auth-api'
+import { loginTC } from '../../auth-reducer'
+
+import style from './LoginForm.module.css'
 
 export const LoginForm = () => {
-    const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch()
 
-    const formik = useFormik(
-        {
-            initialValues: {
-                email: "",
-                password: "",
-                rememberMe: false,
-            },
-            validate: (values: LoginDataType) => {
-                const errors: FormikErrorType = {}
-                if (!values.email) {
-                    errors.email = "Required"
-                } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-                    errors.email = "Invalid email address ⚠"
-                }
-                if (!values.password) {
-                    errors.password = "Required"
-                } else if (values.password.length < 6) {
-                    errors.password = "Password length must be longer 6 symbols ⚠"
-                }
-                return errors
-            },
-            onSubmit: (values: LoginDataType) => {
-                dispatch(loginTC(values))
-                formik.resetForm()
-            },
-        }
-    )
-//console.log(formik.values)
-//console.log(formik.errors)
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      rememberMe: false,
+    },
+    validate: (values: LoginDataType) => {
+      const errors: FormikErrorType = {}
 
+      if (!values.email) {
+        errors.email = 'Required'
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address ⚠'
+      }
+      if (!values.password) {
+        errors.password = 'Required'
+      } else if (values.password.length < 6) {
+        errors.password = 'Password length must be longer 6 symbols ⚠'
+      }
 
-    return (
-        <form onSubmit={formik.handleSubmit}>
-            <FormGroup>
-                <TextField label="Email"
-                           size={"small"}
-                           margin={"normal"}
-                           variant={"standard"}
-                           {...formik.getFieldProps("email")}/>
-                {formik.errors.email &&
-                    <div className={style.error}>
-                        {formik.touched.email && formik.errors.email}
-                    </div>}
-                <TextField label="Password"
-                           type={"password"}
-                           size={"small"}
-                           margin={"normal"}
-                           variant={"standard"}
-                           {...formik.getFieldProps("password")}/>
-                {formik.touched.password && formik.errors.password &&
-                    <div className={style.error}>
-                        {formik.errors.password}
-                    </div>}
-                <div className={style.checkbox}>
-                    <FormControlLabel label={"Remember me"}
-                                      control={<Checkbox checked={formik.values.rememberMe}
-                                                         {...formik.getFieldProps("rememberMe")}/>}/>
-                </div>
-                <div className={style.linkPassword}>
-                    <NavLink to={"/passwordRecovery"}>Forgot password?</NavLink>
-                </div>
-                <Button
-                    type={"submit"}
-                    variant="contained"
-                    sx={{
-                        borderRadius: "50px",
-                        fontFamily: "Montserrat, sans-serif",
-                        fontWeight: "300"
-                    }}>Sign In</Button>
-                <div className={style.textRegister}>Dont have an account?</div>
-                <div className={style.linkRegister}>
-                    <NavLink to={"/registration"}>Sign Up</NavLink>
-                </div>
-            </FormGroup>
-        </form>
-    );
-};
+      return errors
+    },
+    onSubmit: (values: LoginDataType) => {
+      dispatch(loginTC(values))
+      formik.resetForm()
+    },
+  })
+  //console.log(formik.values)
+  //console.log(formik.errors)
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <FormGroup>
+        <TextField
+          label="Email"
+          size={'small'}
+          margin={'normal'}
+          variant={'standard'}
+          {...formik.getFieldProps('email')}
+        />
+        {formik.errors.email && (
+          <div className={style.error}>{formik.touched.email && formik.errors.email}</div>
+        )}
+        <TextField
+          label="Password"
+          type={'password'}
+          size={'small'}
+          margin={'normal'}
+          variant={'standard'}
+          {...formik.getFieldProps('password')}
+        />
+        {formik.touched.password && formik.errors.password && (
+          <div className={style.error}>{formik.errors.password}</div>
+        )}
+        <div className={style.checkbox}>
+          <FormControlLabel
+            label={'Remember me'}
+            control={
+              <Checkbox
+                checked={formik.values.rememberMe}
+                {...formik.getFieldProps('rememberMe')}
+              />
+            }
+          />
+        </div>
+        <div className={style.linkPassword}>
+          <NavLink to={'/passwordRecovery'}>Forgot password?</NavLink>
+        </div>
+        <Button
+          type={'submit'}
+          variant="contained"
+          sx={{
+            borderRadius: '50px',
+            fontFamily: 'Montserrat, sans-serif',
+            fontWeight: '300',
+          }}
+        >
+          Sign In
+        </Button>
+        <div className={style.textRegister}>Dont have an account?</div>
+        <div className={style.linkRegister}>
+          <NavLink to={'/registration'}>Sign Up</NavLink>
+        </div>
+      </FormGroup>
+    </form>
+  )
+}
 
 ///----------- types -----------\\\
 type FormikErrorType = {
-    email?: string,
-    password?: string,
-    rememberMe?: boolean
+  email?: string
+  password?: string
+  rememberMe?: boolean
 }
