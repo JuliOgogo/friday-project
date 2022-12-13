0590959866c6bfd0c26d52b0ee67943fc827fa2b
Diana Lipanova <altere2804@gmail.com>
2022-12-13 11:48:55 +0100

    create NewPassword logic

--- src/features/old/pages/NewPassword.tsx
+++ src/features/old/pages/NewPassword.tsx
@@ -1,11 +1,55 @@
 import React from "react";
+import {Button, FormGroup, TextField} from "@mui/material";
+import {useFormik} from "formik";
 
 type NewPasswordRecoveryPropsType = {}
 
 export const NewPassword: React.FC<NewPasswordRecoveryPropsType> = ({}) => {
+    type FormikErrorType = {
+        password?: string
+    }
+    const formik = useFormik
+    ({
+        initialValues: {
+            password: '',
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {}
+            if (!values.password) {
+                errors.password = 'Required field'
+            } else if (values.password.length < 8) {
+                errors.password = 'Invalid password length! It should be more then 7 symbols!'
+            }
+
+            return errors
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values))
+            formik.resetForm()
+        },
+    })
+
     return (
-        <div>
-            New Password
-        </div>
+        <form onSubmit={formik.handleSubmit}>
+            <FormGroup >
+                <h3>Create new password</h3>
+
+                <TextField type="password"
+                           label="Password"
+                           margin="normal"
+                           {...formik.getFieldProps('password')}
+                           onBlur={formik.handleBlur}
+                />
+                {formik.touched.password && formik.errors.password
+                    ? <div style={{color: 'red'}}>{formik.errors.password}</div>
+                    : null}
+
+                <p>Create new password and we will send you further instructions to email</p>
+
+                <Button type={'submit'} variant={'contained'} color={'primary'}>
+                    Create new password
+                </Button>
+            </FormGroup>
+        </form>
     )
 }
