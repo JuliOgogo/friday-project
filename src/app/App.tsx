8c8bbd10c0fed99f218ca2117b0e9f54e57a4594
Diana Lipanova <altere2804@gmail.com>
2022-12-13 23:24:07 +0100

    add provider to app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css'
 import {Test} from '../features/old/Test'
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 function App() {
     return (
+      <Provider store={store}>
         <div className="App">
-            <Test/>
+          <Test />
         </div>
+      </Provider>
     )
 }
 
