5d4d77887b6e0165694e0b266f7f33fcae8e01bb
Diana Lipanova <altere2804@gmail.com>
2022-12-13 11:59:08 +0100

    add AppThunk type

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,24 @@
 import { appReducer } from './app-reducer'
 import { AnyAction, applyMiddleware, combineReducers, legacy_createStore } from 'redux'
-import thunk, { ThunkDispatch } from 'redux-thunk'
+import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import {PasswordActionsType, passwordReducer} from "../features/old/pages/password/password-reducer";
 
 const reducers = combineReducers({
   app: appReducer,
+  password: passwordReducer,
 })
 
 const store = legacy_createStore(reducers, applyMiddleware(thunk))
 
 export default store
 
+// types
 export type AppStoreType = ReturnType<typeof reducers>
+export type AppActionsType = PasswordActionsType
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppStoreType, unknown, AppActionsType>
+
+// hooks
 export const useAppDispatch = () => useDispatch<ThunkDispatch<AppStoreType, unknown, AnyAction>>()
 export const useAppSelector: TypedUseSelectorHook<AppStoreType> = useSelector
 
