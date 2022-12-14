905a63c0625555e2aefb9caff914f89a8b7037fc
Julia Titkova <juli.ogogo@yandex.ru>
2022-12-14 20:43:46 +0300

    merge visiblePassword2.0

--- src/features/auth/login/loginForm/LoginForm.tsx
+++ src/features/auth/login/loginForm/LoginForm.tsx
@@ -1,83 +1,84 @@
-import React from "react";
+import React from 'react'
 
+import { Visibility, VisibilityOff } from '@mui/icons-material'
 import {
-  Button, Checkbox, FormControl, FormControlLabel, FormGroup,
-  IconButton, Input, InputAdornment, InputLabel, TextField
-} from "@mui/material";
-import { useFormik } from "formik";
-import { NavLink } from "react-router-dom";
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  TextField,
+} from '@mui/material'
+import { useFormik } from 'formik'
+import { NavLink } from 'react-router-dom'
 
-import { useAppDispatch } from "../../../../app/store";
-import { forgotPassword } from "../../../../common/routes/pathRoutesList";
-import { LoginDataType } from "../../auth-api";
-import { setLoginTC } from "../../auth-reducer";
+import { useAppDispatch } from '../../../../app/store'
+import { forgotPassword } from '../../../../common/routes/pathRoutesList'
+import { LoginDataType } from '../../auth-api'
+import { setLoginTC } from '../../auth-reducer'
 
-import style from "./LoginForm.module.css";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
+import style from './LoginForm.module.css'
 
 export const LoginForm = () => {
-  const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch()
 
-  const [showPassword, setShowPassword] = React.useState(false);
+  const [showPassword, setShowPassword] = React.useState(false)
 
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleClickShowPassword = () => setShowPassword(show => !show)
 
-  const handleMouseDownPassword = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-  };
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+  }
 
   const formik = useFormik({
     initialValues: {
-      email: "",
-      password: "",
-      rememberMe: false
+      email: '',
+      password: '',
+      rememberMe: false,
     },
     validate: (values: LoginDataType) => {
-      const errors: FormikErrorType = {};
+      const errors: FormikErrorType = {}
 
       if (!values.email) {
-        errors.email = "Required";
+        errors.email = 'Required'
       } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = "Invalid email address ⚠";
+        errors.email = 'Invalid email address ⚠'
       }
       if (!values.password) {
-        errors.password = "Required";
+        errors.password = 'Required'
       } else if (values.password.length < 7) {
-        errors.password = "Password length must be longer 6 symbols ⚠";
+        errors.password = 'Password length must be longer 6 symbols ⚠'
       }
 
-      return errors;
+      return errors
     },
     onSubmit: (values: LoginDataType) => {
-      dispatch(setLoginTC(values));
-      formik.resetForm();
-    }
-  });
-  //console.log(formik.values)
-  //console.log(formik.errors)
+      dispatch(setLoginTC(values))
+      formik.resetForm()
+    },
+  })
 
   return (
     <form onSubmit={formik.handleSubmit}>
       <FormGroup>
         <TextField
           label="Email"
-          size={"small"}
-          margin={"normal"}
-          variant={"standard"}
-          {...formik.getFieldProps("email")}
+          size={'small'}
+          margin={'normal'}
+          variant={'standard'}
+          {...formik.getFieldProps('email')}
         />
         {formik.errors.email && (
           <div className={style.error}>{formik.touched.email && formik.errors.email}</div>
         )}
         <FormControl variant="standard">
-          <InputLabel htmlFor="standard-adornment-password">
-            Password
-          </InputLabel>
+          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
           <Input
             id="standard-adornment-password"
-            type={showPassword ? "text" : "password"}
+            type={showPassword ? 'text' : 'password'}
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
@@ -89,7 +90,7 @@ export const LoginForm = () => {
                 </IconButton>
               </InputAdornment>
             }
-            {...formik.getFieldProps("password")}
+            {...formik.getFieldProps('password')}
           />
           {formik.touched.password && formik.errors.password && (
             <div className={style.error}>{formik.errors.password}</div>
@@ -97,11 +98,11 @@ export const LoginForm = () => {
         </FormControl>
         <div className={style.checkbox}>
           <FormControlLabel
-            label={"Remember me"}
+            label={'Remember me'}
             control={
               <Checkbox
                 checked={formik.values.rememberMe}
-                {...formik.getFieldProps("rememberMe")}
+                {...formik.getFieldProps('rememberMe')}
               />
             }
           />
@@ -109,17 +110,17 @@ export const LoginForm = () => {
         <div className={style.linkPassword}>
           <NavLink to={forgotPassword}>Forgot password?</NavLink>
         </div>
-        <button className={style.buttonSubmit} type={"submit"}>
+        <button className={style.buttonSubmit} type={'submit'}>
           Sign In
         </button>
         <div className={style.textRegister}>Dont have an account?</div>
         <div className={style.linkRegister}>
-          <NavLink to={"/registration"}>Sign Up</NavLink>
+          <NavLink to={'/registration'}>Sign Up</NavLink>
         </div>
       </FormGroup>
     </form>
-  );
-};
+  )
+}
 
 ///----------- types -----------\\\
 type FormikErrorType = {
