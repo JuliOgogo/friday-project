b966bdb24a2444397479ef2c1c3179e5a72ec0c2
Julia Titkova <juli.ogogo@yandex.ru>
2022-12-15 19:51:26 +0300

    merge isLoggedIn

--- src/index.tsx
+++ src/index.tsx
@@ -12,13 +12,11 @@ import reportWebVitals from './reportWebVitals'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-
-    <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Provider>
-
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
 )
 
 // If you want to start measuring performance in your app, pass a function
