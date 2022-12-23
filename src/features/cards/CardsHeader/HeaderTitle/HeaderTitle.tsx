31cd38589d2c6eca08f7b69681b2598a8a533ce8
Diana Lipanova <altere2804@gmail.com>
2022-12-23 12:34:31 +0100

    add: addButton logic

--- src/features/cards/CardsHeader/HeaderTitle/HeaderTitle.tsx
+++ src/features/cards/CardsHeader/HeaderTitle/HeaderTitle.tsx
@@ -1,18 +1,34 @@
 import React from 'react'
 
 import Button from '@mui/material/Button'
+import { useParams } from 'react-router-dom'
 
-import { useAppSelector } from '../../../../app/store'
+import { useAppDispatch, useAppSelector } from '../../../../app/store'
 import { CustomMenu } from '../../../../common/components/CustomMenu/CustomMenu'
 import { userId } from '../../../auth/auth-selector'
+import { addCardTC } from '../../cards-reducer'
 import { packUserId } from '../../cards-selector'
 
 import s from './HeaderTitle.module.css'
 
 export const HeaderTitle = () => {
+  const dispatch = useAppDispatch()
+  const { id_pack } = useParams()
+
   const myId = useAppSelector(userId)
   const friendsId = useAppSelector(packUserId)
   const title = myId === friendsId ? 'My Pack' : "Friend's Pack"
+
+  const addNewCardHandler = () => {
+    dispatch(
+      addCardTC({
+        cardsPack_id: id_pack ? id_pack : '',
+        question: 'New Question',
+        answer: 'New Answer',
+        grade: 2,
+      })
+    )
+  }
 
   return (
     <div className={s.titleAndButton}>
@@ -21,7 +37,7 @@ export const HeaderTitle = () => {
         {title === 'My Pack' && <CustomMenu />}
       </div>
       {title === 'My Pack' ? (
-        <Button variant={'contained'} sx={{ borderRadius: '30px' }}>
+        <Button variant={'contained'} sx={{ borderRadius: '30px' }} onClick={addNewCardHandler}>
           Add new card
         </Button>
       ) : (
