a8c8669986bc2a3ccc6aff5a0057bf6202320754
Julia Titkova <juli.ogogo@yandex.ru>
2022-12-23 17:49:53 +0300

    refactor: url heroku

--- src/features/cards/cards-api.ts
+++ src/features/cards/cards-api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
-  // baseURL: process.env.REACT_APP_BACK_URL || 'https://neko-back.herokuapp.com/2.0/cards/card',
+  // baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+  baseURL: process.env.REACT_APP_BACK_URL || 'https://neko-back.herokuapp.com/2.0/cards/card',
 
   // baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/cards/card' : 'https://neko-back.herokuapp.com/2.0/cards/card',
   withCredentials: true,
